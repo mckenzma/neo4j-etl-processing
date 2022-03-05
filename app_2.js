8fb1166298345d8b7a5e921b579662f8394424fb
Michael McKenzie <mckenzma@gmail.com>
2022-03-04 20:41:21 -0500

    Add results to query output

--- app_2.js
+++ app_2.js
@@ -45,48 +45,28 @@ const asyncFunction = async (query,j) => {
     await session
     .run(query)
     .then(result => {
-      // console.log(result);
-
-      // result.records.forEach(record => {
-      //   // console.log(record);
-      //   var obj = {};
-
-      //   for(var i=0;i<record.keys.length;i++){
-      //     // console.log(etl[queryFiles[j]]);
-      //     obj[record.keys[i]] = record._fields[i];
-
-      //   }
-      //   // console.log(obj);
-      //   // etl.queries[j].output = obj;
-      // })
-
       const end = new Date().toISOString();
-      // console.log(start,end, Date());
 
-      // etl.queries[j].result = result;
+      // TODO need to format result better
       etl.queries[j].start = start;
       etl.queries[j].end = end;
-      // etl.queries[j].status = 'Complete'
+      etl.queries[j].status = 'Complete'
+      // This is the easiest way but seems expensive in terms of keeping all of the info
+      etl.queries[j].result = result;
       generateFile(file);
     })
     .catch(error => {
-      console.log(error)
-      // etl.queries[j].status = 'Error'
-      // etl.queries[j].result = error;
-      // generateFile(file);
+      const end = new Date().toISOString();
+      etl.queries[j].start = start;
+      etl.queries[j].end = end;
+      etl.queries[j].status = 'Error';
+      // can we extract extra info regarding specifics about the error
+      etl.queries[j].result = { error, message: error.message, query: query }
     })
     .then(() => session.close())
-
 }
 
 for (var j=0;j<queryFiles.length;j++) {
-  // console.log(j, queryFiles[j]);
-  
-  // check if status says it can run
-  // What are the statuses?
-
-  // get query from file
-  // var query = fs.readFileSync('./queries/' + queryFiles[j].file);
   const query = fs.readFileSync('./cypher/' + queryFiles[j].file, 'utf-8').toString();
   
   asyncFunction(query,j);
